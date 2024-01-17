0bd3be7736df2fb34323bd3a6e1804b59ef53ee2
John McElhatton <jmcelhatton2010@gmail.com>
2024-01-17 16:31:52 -0500

    Initial Commit

---
+++ Resume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Resume
+//
+//  Created by John McElhatton on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by John McElhatton on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
