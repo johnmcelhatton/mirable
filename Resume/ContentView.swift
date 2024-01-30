a96496cf71802f70c4f7bd69a49380d91523e983
John McElhatton <jmcelhatton2010@gmail.com>
2024-01-30 18:37:16 -0500

    new commit

--- Resume/ContentView.swift
+++ Resume/ContentView.swift
@@ -8,17 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    VStack(alignment:.leading,spacing:0) { //Brings Interests closer to emojis
+      Text("John McElhatton")
+        .font(.largeTitle)
+      
+      Spacer()
+      
+      Group{
+        Text("Work Experience")
+          .font(.title)
+        Rectangle()
+          .frame(height:1) //makes a line across
+          .padding(.bottom)
+        Spacer()
+      } //Work Experience group
+      
+      Group { Text("Education")
+          .font(.title)
+        Rectangle()
+          .frame(height:1) //makes a line across
+          .padding(.bottom)
+        Text("Bachelors Degree")
+        Text("Villanova")
+        Text("1966- 1970")
+        Text ("English")
+        Spacer()
+      } //Education Group
+      
+      Group{ Text("Interests")
+        .font(.title)
+      Rectangle()
+        .frame(height:1) //makes a line across
+        .padding(.bottom)      //set a direction for padding()
+        HStack {
+          Spacer()
+          
+          VStack {
+            Text("🏈")
+              .font(.system(size:70))
+            Text("Football")
+              .bold()
+          }
+          Spacer()
+          
+          VStack {
+            Text("💾")
+              .font(.system(size: 70))
+            
+            Text("Coding")
+              .bold()
+          }
+          Spacer()
+          VStack{
+            Text("🏌️")
+              .font(.system(size: 70))
+            Text("Golf")
+              .bold()
+          }
+          
+          Spacer()
+            .font(.system(size:70))
+          Spacer()
+          
         }
-        .padding()
+      } //Interests Group
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
